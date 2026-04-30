1760493df10c319e135d9d4a88959a921fd86a3c
waqar-ukaea <waqar.butt@ukaea.uk>
2026-04-30 15:08:23 +0100

    Copy vertices+indices to device and build bvh

--- src/cuBQL/ray_tracer.cpp
+++ src/cuBQL/ray_tracer.cpp
@@ -35,23 +35,27 @@ CuBQLRayTracer::create_surface_tree(const std::shared_ptr<MeshManager>& mesh_man
 {
 
   int gpu_id = 0; // TODO - how to manage GPU IDs in a multi-GPU system?
+  int host_id = omp_get_initial_device();
 
-  cuBQL::box3d *d_boxes = nullptr; // box3d is an alias for box_t<double, 3>
-  int num_boxes = 0;
 
-  cuBQL::bvh3d bvh; // bvh3d is an alias for BinaryBVH<double, 3> 
-  // bvh_t is an alias for BinaryBVH<T, D>, so bvh_t<double, 3> is also BinaryBVH<double, 3>
+  // cuBQL::box3d *d_boxes = nullptr; // box3d is an alias for box_t<double, 3>
+  // int num_boxes = 0;
+
+  // cuBQL::bvh3d bvh; // bvh3d is an alias for BinaryBVH<double, 3> 
+  // // bvh_t is an alias for BinaryBVH<T, D>, so bvh_t<double, 3> is also BinaryBVH<double, 3>
 
   cuBQL::BuildConfig buildParams; // It looks like spatial median is the only supported method for omp builder
-  // Looks like the cuBQL::gpuBuilder is only pulled in when cubql is built with CUDA support 
+  // // Looks like the cuBQL::gpuBuilder is only pulled in when cubql is built with CUDA support 
   
-  cuBQL::build_omp_target(bvh, d_boxes, num_boxes, buildParams, gpu_id); // build bvh on GPU 0 using OpenMP target offloading
+  // cuBQL::build_omp_target(bvh, d_boxes, num_boxes, buildParams, gpu_id); // build bvh on GPU 0 using OpenMP target offloading
 
   SurfaceTreeID tree = next_surface_tree_id();
   surface_trees_.push_back(tree);
   auto volume_surfaces = mesh_manager->get_volume_surfaces(volume_id);
 
   for (const auto &surf : volume_surfaces) {
+    cuBQL::bvh3d bvh; // bvh3d is an alias for BinaryBVH<double, 3> 
+
     auto num_faces = mesh_manager->num_surface_faces(surf);
     auto vertices = mesh_manager->get_surface_vertices(surf);
     auto indices = mesh_manager->get_surface_connectivity(surf);
@@ -73,9 +77,13 @@ CuBQLRayTracer::create_surface_tree(const std::shared_ptr<MeshManager>& mesh_man
     // copy vertices and indices to device
     auto* d_vertices = static_cast<cuBQL::vec3d*>
       (omp_target_alloc(h_vertices.size() * sizeof(cuBQL::vec3d), gpu_id));
+    omp_target_memcpy(d_vertices, h_vertices.data(), 
+      h_vertices.size() * sizeof(cuBQL::vec3d), 0, 0, gpu_id, host_id);
 
     auto* d_indices = static_cast<cuBQL::vec3i*>
       (omp_target_alloc(h_indices.size() * sizeof(cuBQL::vec3i), gpu_id));
+    omp_target_memcpy(d_indices, h_indices.data(), 
+      h_indices.size() * sizeof(cuBQL::vec3i), 0, 0, gpu_id, host_id);
 
     // Create device storage for triangle AABBs to be computed in parallel on GPU
     auto* d_aabbs = static_cast<cuBQL::box3d*>
@@ -86,7 +94,7 @@ CuBQLRayTracer::create_surface_tree(const std::shared_ptr<MeshManager>& mesh_man
     // TODO - How can this be extended for tets and other element types?
     #pragma omp target device(gpu_id) is_device_ptr(d_vertices, d_indices, d_aabbs)
     #pragma omp teams distribute parallel for
-    for (uint32_t primID = 0; primID < num_faces; ++primID) {
+    for (uint32_t primID = 0; primID < h_indices.size(); ++primID) {
       cuBQL::vec3i indices = d_indices[primID];
 
       cuBQL::vec3d A = d_vertices[indices.x];
@@ -99,7 +107,11 @@ CuBQLRayTracer::create_surface_tree(const std::shared_ptr<MeshManager>& mesh_man
       aabb.extend(C);
 
       d_aabbs[primID] = aabb;
-    }
+    } // This is our AABB population kernel written with OpenMP target offloading pragmas to run in parallel on the GPU
+
+    // Construct the bvh on the gpu using the AABBs with openmp pathway
+    cuBQL::build_omp_target(bvh, d_aabbs, num_faces, buildParams, gpu_id); 
+    
   }
   return TREE_NONE;
 }
