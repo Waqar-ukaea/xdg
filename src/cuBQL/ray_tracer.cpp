cb7fee9989f10fd235447b2b0e1890b6a9d5055d
waqar-ukaea <waqar.butt@ukaea.uk>
2026-06-15 17:21:53 +0100

    Some minor cleanup

--- src/cuBQL/ray_tracer.cpp
+++ src/cuBQL/ray_tracer.cpp
@@ -47,10 +47,8 @@ void CuBQLRayTracer::init()
 
 void CuBQLRayTracer::upload_volume_to_tlas_table_()
 {
-  const int gpu_id = context_.gpuID;
-
   if (d_volume_to_tlas_) {
-    omp_target_free(d_volume_to_tlas_, gpu_id);
+    omp_target_free(d_volume_to_tlas_, context_.gpuID);
     d_volume_to_tlas_ = nullptr;
   }
 
@@ -59,13 +57,13 @@ void CuBQLRayTracer::upload_volume_to_tlas_table_()
   }
 
   d_volume_to_tlas_ = static_cast<CuBQLVolumeTLAS::DD*>
-    (omp_target_alloc(volume_to_tlas_.size() * sizeof(CuBQLVolumeTLAS::DD), gpu_id));
+    (omp_target_alloc(volume_to_tlas_.size() * sizeof(CuBQLVolumeTLAS::DD), context_.gpuID));
   omp_target_memcpy(d_volume_to_tlas_,
                     volume_to_tlas_.data(),
                     volume_to_tlas_.size() * sizeof(CuBQLVolumeTLAS::DD),
                     0,
                     0,
-                    gpu_id,
+                    context_.gpuID,
                     context_.hostID);
 }
 
@@ -82,8 +80,6 @@ CuBQLSurfaceBLAS
 CuBQLRayTracer::register_surface(const std::shared_ptr<MeshManager>& mesh_manager,
                                   MeshID surface_id)
 {
-  const auto& context = context_;
-  const int gpu_id = context.gpuID;
   auto num_faces = mesh_manager->num_surface_faces(surface_id);
   auto vertices = mesh_manager->get_surface_vertices(surface_id);
   auto indices = mesh_manager->get_surface_connectivity(surface_id);
@@ -104,41 +100,41 @@ CuBQLRayTracer::register_surface(const std::shared_ptr<MeshManager>& mesh_manage
 
   // TODO- think about how to better handle omp transfer calls. AutoUploadArrays is one option
   auto* d_vertices = static_cast<cuBQL::vec3d*>
-    (omp_target_alloc(h_vertices.size() * sizeof(cuBQL::vec3d), gpu_id));
+    (omp_target_alloc(h_vertices.size() * sizeof(cuBQL::vec3d), context_.gpuID));
   omp_target_memcpy(d_vertices,
                     h_vertices.data(),
                     h_vertices.size() * sizeof(cuBQL::vec3d),
                     0,
                     0,
-                    gpu_id,
-                    context.hostID);
+                    context_.gpuID,
+                    context_.hostID);
 
   auto* d_indices = static_cast<cuBQL::vec3i*>
-    (omp_target_alloc(h_indices.size() * sizeof(cuBQL::vec3i), gpu_id));
+    (omp_target_alloc(h_indices.size() * sizeof(cuBQL::vec3i), context_.gpuID));
   omp_target_memcpy(d_indices,
                     h_indices.data(),
                     h_indices.size() * sizeof(cuBQL::vec3i),
                     0,
                     0,
-                    gpu_id,
-                    context.hostID);
+                    context_.gpuID,
+                    context_.hostID);
 
   auto* d_primitive_refs = static_cast<MeshID*>
-    (omp_target_alloc(h_primitive_refs.size() * sizeof(MeshID), gpu_id));
+    (omp_target_alloc(h_primitive_refs.size() * sizeof(MeshID), context_.gpuID));
   omp_target_memcpy(d_primitive_refs,
                     h_primitive_refs.data(),
                     h_primitive_refs.size() * sizeof(MeshID),
                     0,
                     0,
-                    gpu_id,
-                    context.hostID);
+                    context_.gpuID,
+                    context_.hostID);
 
   auto* d_aabbs = static_cast<cuBQL::box3d*>
-    (omp_target_alloc(h_indices.size() * sizeof(cuBQL::box3d), gpu_id));
+    (omp_target_alloc(h_indices.size() * sizeof(cuBQL::box3d), context_.gpuID));
   const auto num_primitives = static_cast<uint32_t>(h_indices.size());
 
   // TODO - Abstract this out into its own bounding_box creation function
-  #pragma omp target device(gpu_id) is_device_ptr(d_vertices, d_indices, d_aabbs)
+  #pragma omp target device(context_.gpuID) is_device_ptr(d_vertices, d_indices, d_aabbs)
   #pragma omp teams distribute parallel for
   for (uint32_t primID = 0; primID < num_primitives; ++primID) {
     cuBQL::vec3i indices = d_indices[primID];
@@ -159,9 +155,9 @@ CuBQLRayTracer::register_surface(const std::shared_ptr<MeshManager>& mesh_manage
   // TODO - Try setting leaf params to 1 to see what it does
   // Check what default is for CUDA 
   cuBQL::bvh3d bvh;
-  cuBQL::build_omp_target(bvh, d_aabbs, num_faces, blasBuildParams, gpu_id);
+  cuBQL::build_omp_target(bvh, d_aabbs, num_faces, blasBuildParams, context_.gpuID);
 
-  omp_target_free(d_aabbs, gpu_id);
+  omp_target_free(d_aabbs, context_.gpuID);
 
   CuBQLSurfaceMesh surface_mesh;
   surface_mesh.surface_id = surface_id;
@@ -170,13 +166,13 @@ CuBQLRayTracer::register_surface(const std::shared_ptr<MeshManager>& mesh_manage
   surface_mesh.d_primitive_refs = d_primitive_refs;
   surface_mesh.num_vertices = h_vertices.size();
   surface_mesh.num_triangles = num_faces;
-  surface_mesh.gpu_id = gpu_id;
+  surface_mesh.gpu_id = context_.gpuID;
 
   CuBQLSurfaceBLAS surface_blas;
   surface_blas.bvh = bvh;
   surface_blas.mesh = surface_mesh;
   surface_blas.num_prims = num_faces;
-  surface_blas.gpu_id = gpu_id;
+  surface_blas.gpu_id = context_.gpuID;
 
   return surface_blas;
 }
@@ -185,22 +181,8 @@ TreeID
 CuBQLRayTracer::create_surface_tree(const std::shared_ptr<MeshManager>& mesh_manager,
                                      MeshID volume_id)
 {
-
-  const auto& context = context_;
-  const int gpu_id = context.gpuID;
-
-
-  // cuBQL::box3d *d_boxes = nullptr; // box3d is an alias for box_t<double, 3>
-  // int num_boxes = 0;
-
-  // cuBQL::bvh3d bvh; // bvh3d is an alias for BinaryBVH<double, 3> 
-  // bvh_t is an alias for BinaryBVH<T, D>, so bvh_t<double, 3> is also BinaryBVH<double, 3>
-
-  // Looks like the cuBQL::gpuBuilder is only pulled in when cubql is built with CUDA support 
-
-  // It looks like spatial median is the only supported method for omp builder
-
-  // cuBQL::build_omp_target(bvh, d_boxes, num_boxes, buildParams, gpu_id); // build bvh on GPU 0 using OpenMP target offloading
+  // TODO - Right now each CuBQLRayTracer instance has a single "Context" which holds a single GPU_ID
+  // so this will need to be reworked in the future to handle multi-gpus
 
   SurfaceTreeID tree = next_surface_tree_id();
   surface_trees_.push_back(tree);
@@ -249,24 +231,24 @@ CuBQLRayTracer::create_surface_tree(const std::shared_ptr<MeshManager>& mesh_man
   }
 
   auto* d_tlas_boxes = static_cast<cuBQL::box3d*>
-    (omp_target_alloc(h_tlas_boxes.size() * sizeof(cuBQL::box3d), gpu_id));
+    (omp_target_alloc(h_tlas_boxes.size() * sizeof(cuBQL::box3d), context_.gpuID));
   omp_target_memcpy(d_tlas_boxes,
                     h_tlas_boxes.data(),
                     h_tlas_boxes.size() * sizeof(cuBQL::box3d),
                     0,
                     0,
-                    gpu_id,
-                    context.hostID);
+                    context_.gpuID,
+                    context_.hostID);
 
   auto* d_surface_instances = static_cast<CuBQLVolumeTLAS::SurfaceInstanceDD*>
-    (omp_target_alloc(h_surface_instances.size() * sizeof(CuBQLVolumeTLAS::SurfaceInstanceDD), gpu_id));
+    (omp_target_alloc(h_surface_instances.size() * sizeof(CuBQLVolumeTLAS::SurfaceInstanceDD), context_.gpuID));
   omp_target_memcpy(d_surface_instances,
                     h_surface_instances.data(),
                     h_surface_instances.size() * sizeof(CuBQLVolumeTLAS::SurfaceInstanceDD),
                     0,
                     0,
-                    gpu_id,
-                    context.hostID);
+                    context_.gpuID,
+                    context_.hostID);
 
   cuBQL::BuildConfig tlasBuildParams;
   tlasBuildParams.makeLeafThreshold = 1;
@@ -275,15 +257,15 @@ CuBQLRayTracer::create_surface_tree(const std::shared_ptr<MeshManager>& mesh_man
   CuBQLVolumeTLAS volume_tlas;
   volume_tlas.volume_id = volume_id; // store meshid in the TLAS object for easier mapping between the two
   volume_tlas.num_surface_instances = static_cast<uint32_t>(h_surface_instances.size());
-  volume_tlas.gpu_id = gpu_id;
+  volume_tlas.gpu_id = context_.gpuID;
   volume_tlas.d_surface_instances = d_surface_instances;
   cuBQL::build_omp_target(volume_tlas.bvh,
                           d_tlas_boxes,
                           volume_tlas.num_surface_instances,
                           tlasBuildParams,
-                          gpu_id);
+                          context_.gpuID);
 
-  omp_target_free(d_tlas_boxes, gpu_id);
+  omp_target_free(d_tlas_boxes, context_.gpuID);
 
   // Still required for lifetime and scalar calls which need to resolve TreeID->volume_tlas on CPU side.
   auto result = tree_to_volume_tlas_.emplace(tree, std::move(volume_tlas));
