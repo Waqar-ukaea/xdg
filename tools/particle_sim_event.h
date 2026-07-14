dcd15727f1be4014bda1d1442ec4659c6b4c4e21
waqar-ukaea <waqar.butt@ukaea.uk>
2026-07-14 15:05:01 +0100

    Implemented process_surface_crossing_events()

--- tools/particle_sim_event.h
+++ tools/particle_sim_event.h
@@ -157,6 +157,11 @@ struct EventParticle {
     surface_hit_distance_ = INFTY;
     collision_distance_ = INFTY;
     last_surface_hit_ = ID_NONE;
+    next_volume_ = ID_NONE;
+    boundary_condition_ = UNSET;
+    surface_normal_.x = 0.0;
+    surface_normal_.y = 0.0;
+    surface_normal_.z = 0.0;
     rng_state_ = seed ^ id;
     n_events_ = 0;
     alive_ = true;
@@ -201,15 +206,79 @@ struct EventParticle {
     surface_hit_ = ID_NONE;
     surface_hit_distance_ = INFTY;
     last_surface_hit_ = ID_NONE;
+    next_volume_ = ID_NONE;
+    boundary_condition_ = UNSET;
+    surface_normal_.x = 0.0;
+    surface_normal_.y = 0.0;
+    surface_normal_.z = 0.0;
   }
 
-  void store_surface_hit(MeshID surface, double distance)
+  void store_surface_crossing(MeshID surface,
+                              double distance,
+                              MeshID next_volume,
+                              SurfaceBoundaryCondition boundary_condition,
+                              const double normal[3])
   {
     surface_hit_ = surface;
     surface_hit_distance_ = distance;
     last_surface_hit_ = surface;
+    next_volume_ = next_volume;
+    boundary_condition_ = boundary_condition;
+    surface_normal_.x = normal[0];
+    surface_normal_.y = normal[1];
+    surface_normal_.z = normal[2];
   }
 
+  void surface_cross()
+  {
+    n_events_++;
+
+    switch (boundary_condition_) {
+    case SurfaceBoundaryCondition::TRANSMISSION:
+      volume_ = next_volume_;
+
+      // TODO: Restore optional implicit-complement graveyard handling.
+      if (volume_ == ID_NONE) {
+        alive_ = false;
+      }
+      break;
+
+    case SurfaceBoundaryCondition::VACUUM:
+      alive_ = false;
+      break;
+
+    case SurfaceBoundaryCondition::REFLECTIVE: {
+      const double nx = surface_normal_.x;
+      const double ny = surface_normal_.y;
+      const double nz = surface_normal_.z;
+
+      // cuBQL returns the raw triangle normal. Dividing by n dot n applies
+      // the reflection formula without first normalising meaning we skip a square root.
+      const double normal_squared = nx * nx + ny * ny + nz * nz;
+      const double projection = u_.x * nx + u_.y * ny + u_.z * nz;
+      const double scale = 2.0 * projection / normal_squared;
+
+      u_.x -= scale * nx;
+      u_.y -= scale * ny;
+      u_.z -= scale * nz;
+
+      // Normalize the reflected particle direction.
+      const double direction_squared = u_.x * u_.x + u_.y * u_.y + u_.z * u_.z;
+      const double inverse_direction_length = 1.0 / std::sqrt(direction_squared);
+      u_.x *= inverse_direction_length;
+      u_.y *= inverse_direction_length;
+      u_.z *= inverse_direction_length;
+      break;
+    }
+
+    case SurfaceBoundaryCondition::UNSET:
+    default:
+      alive_ = false;
+      break;
+    }
+  }
+
+  // Data members
   uint32_t id_ {0};
   Position r_ {};
   Direction u_ {};
@@ -219,6 +288,9 @@ struct EventParticle {
   double surface_hit_distance_ {INFTY};
   double collision_distance_ {INFTY};
   MeshID last_surface_hit_ {ID_NONE};
+  MeshID next_volume_ {ID_NONE};
+  SurfaceBoundaryCondition boundary_condition_ {UNSET};
+  Direction surface_normal_ {0.0};
 
   std::uint32_t rng_state_ {0};
   int32_t n_events_ {0};
@@ -234,13 +306,13 @@ struct EventSimulationData {
   std::shared_ptr<XDG> xdg_;
   double mfp_ {1.0};
   std::uint32_t seed_ {42};
-  uint32_t n_particles_ {100000};
+  uint32_t n_particles_ {1000000};
   uint32_t max_events_ {1000};
   bool verbose_particles_ {false};
   bool implicit_complement_is_graveyard_ {false};
   std::unordered_map<MeshID, double> cell_tracks;
 
-  uint32_t max_particles_in_flight_ {100000};
+  uint32_t max_particles_in_flight_ {1000000};
   EventParticle* device_particles {nullptr};
   int gpu_id {0};
   int host_id {omp_get_initial_device()};
@@ -396,13 +468,19 @@ inline void process_advance_particle_events(EventSimulationData& sim_data)
     ray_hits[i].direction[0] = p.u_.x;
     ray_hits[i].direction[1] = p.u_.y;
     ray_hits[i].direction[2] = p.u_.z;
-    ray_hits[i].t_min = 0.0;
+    // Avoid immediately re-hitting the surface from which the particle starts.
+    ray_hits[i].t_min = TINY_BIT;
     ray_hits[i].t_max = INFTY;
     ray_hits[i].volume = p.volume_;
     ray_hits[i].distance = INFTY;
     ray_hits[i].surface = ID_NONE;
     ray_hits[i].primitive = ID_NONE;
     ray_hits[i].point_in_volume = OUTSIDE;
+    ray_hits[i].next_volume = ID_NONE;
+    ray_hits[i].boundary_condition = static_cast<std::int32_t>(UNSET);
+    ray_hits[i].normal[0] = 0.0;
+    ray_hits[i].normal[1] = 0.0;
+    ray_hits[i].normal[2] = 0.0;
   }
 
 
@@ -447,7 +525,12 @@ inline void process_advance_particle_events(EventSimulationData& sim_data)
       continue;
     }
 
-    p.store_surface_hit(hit.surface, hit.distance);
+    p.store_surface_crossing(
+      hit.surface,
+      hit.distance,
+      hit.next_volume,
+      static_cast<SurfaceBoundaryCondition>(hit.boundary_condition),
+      hit.normal);
     p.sample_collision_distance(mfp);
     p.advance();
 
@@ -497,6 +580,41 @@ inline void process_collision_events(EventSimulationData& sim_data)
 
   sim_data.advance_particle_queue.sync_size_device_to_host();
   sim_data.collision_queue.reset();
+}
+
+inline void process_surface_crossing_events(EventSimulationData& sim_data)
+{
+  EventParticle* device_particles = sim_data.device_particles;
+  const int n_surface_crossings = sim_data.surface_crossing_queue.size();
+
+  if (n_surface_crossings == 0) {
+    return;
+  }
+
+  auto advance_queue = sim_data.advance_particle_queue.get_device_data();
+  auto surface_crossing_queue = sim_data.surface_crossing_queue.get_device_data();
+  const int gpu_id = sim_data.gpu_id;
+  const int max_events = sim_data.max_events_;
+
+  #pragma omp target teams distribute parallel for device(gpu_id) \
+  is_device_ptr(device_particles) \
+  firstprivate(advance_queue, surface_crossing_queue, max_events)
+  for (int i = 0; i < n_surface_crossings; i++)
+  {
+    const uint32_t particle_idx = surface_crossing_queue.data[i].idx;
+    EventParticle& p = device_particles[particle_idx];
+    p.surface_cross();
+
+    if (!p.alive_ || p.n_events_ >= max_events) {
+      p.alive_ = false;
+      continue;
+    }
+
+    advance_queue.thread_safe_append({particle_idx});
+  }
+
+  sim_data.advance_particle_queue.sync_size_device_to_host();
+  sim_data.surface_crossing_queue.reset();
 }
 
 // void process_death_events();
